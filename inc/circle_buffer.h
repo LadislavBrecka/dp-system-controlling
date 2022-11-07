d54403f3660b9549b24182d59541beea889a0c06
Ladislav Brečka <lacoobrecka@gmail.com>
2022-11-07 15:17:24 +0100

    chore: remove unused commented code

--- inc/circle_buffer.h
+++ inc/circle_buffer.h
@@ -19,7 +19,7 @@ namespace DT{
 
         void add(double value) 
         {
-            if (idx >= size-1) idx = 0; else idx++;                      
+            if (idx >= size-1) idx = 0; else idx++;                    
             vector[idx] = value;           
         };
 
@@ -30,13 +30,5 @@ namespace DT{
             if (real_index < 0) real_index = size + real_index;
             return vector[real_index];
         }
-
-        // double &operator[] (uint index) {
-        //     if (index > size-1) throw std::out_of_range ("Index is out of range!");
-
-        //     int real_index = idx - index;
-        //     if (real_index < 0) real_index = size + real_index;
-        //     return vector[real_index];
-        // };
     };
 }
