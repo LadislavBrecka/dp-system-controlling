c98e826ce6a0e01496e574dcc0b598a189585a08
Martin Dodek <34284002+dodekm@users.noreply.github.com>
2023-01-02 15:01:35 +0100

    Update circle_buffer.h
    Skontrolujte ci to taktu bude fungovat

--- inc/circle_buffer.h
+++ inc/circle_buffer.h
@@ -6,28 +6,30 @@ namespace DT{
 
     private:
         Eigen::VectorXd vector;
-        uint size;
+        // uint size;
         uint idx;
 
     public:
         CircleBuffer(Eigen::VectorXd data)
         {
             vector = data;
-            size = data.size();
-            idx = UINT_MAX;
+            // size = data.size();  skusme to takto - nevyrabat informacie ktore su obsiahnute v inom objekte
+            idx = vector.size()-1;  //UINT_MAX; nemalo by sa stat nic zle ak to dam takto
         };
 
         void add(double value) 
         {
-            if (idx >= size-1) idx = 0; else idx++;                    
+            //if (idx >= vector.size()-1) idx = 0; else idx++;
+            idx++;
+            idx%=vector.size(); //skuste tento trik s modulo operatorom
             vector[idx] = value;           
         };
 
         double at(uint index)
         {
-            if (index > size-1) throw std::out_of_range ("Index is out of range!");
+            if (index > vector.size()-1) throw std::out_of_range ("Index is out of range!");
             int real_index = idx - index;
-            if (real_index < 0) real_index = size + real_index;
+            if (real_index < 0) real_index = vector.size() + real_index; 
             return vector[real_index];
         }
     };
